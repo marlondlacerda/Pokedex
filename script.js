f4205a4c3861e50ede042e64475cc596a875f070
luisArthurRodriguesDaSilva <luisarthurlards03@gmail.com>
2022-04-07 15:52:24 -0300

    atribuições dinamicas

--- script.js
+++ script.js
@@ -1,3 +1,33 @@
-const { pokemons } = require('./database/api.js');
+console.log(pokemons[0]["hp"]);
+console.log(pokemons.length);
 
-console.log(pokemons[0]["id"]);
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const hp = document.querySelector('#hp');
+const ATACK = document.querySelector('#ATACK');
+const DEFENCE = document.querySelector('#DEFENCE');
+const VELOCITY = document.querySelector('#VELOCITY');
+const TOTAL = document.querySelector('#TOTAL');
+
+//for(var i =0;i<pokemons.length;i++)
+while(true)
+{
+    var i=0;
+    while (i<6)
+    {
+        //await new Promise(r => setTimeout(r, 2000));
+        //await sleep(5);
+        await sleep(1 * 1000);
+        hp.innerHTML =       pokemons[i]["hp"];
+        ATACK.innerHTML =    pokemons[i]["ataque"];
+        DEFENCE.innerHTML =  pokemons[i]["defesa"];
+        VELOCITY.innerHTML = pokemons[i]["velocidade"];
+        TOTAL.innerHTML = pokemons[i]["hp"]+
+        pokemons[i]["ataque"]+
+        pokemons[i]["defesa"]+
+        pokemons[i]["velocidade"];
+        i++;
+    }
+}
--- teste.js
+++
@@ -1,2 +0,0 @@
-const { pokemons } = require('./database/api.js');
-document.getElementById("hp").innerHTML = pokemons[0]["hp"];
